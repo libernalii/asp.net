be0deb66d5793541988ef1925a5c90df491e9aae
libernalii <alinaliberna@gmail.com>
2026-03-05 19:20:50 +0200

    Cat HW

---
+++ Cat/DLL/CatContext.cs
@@ -0,0 +1,35 @@
+﻿using DLL.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DLL
+{
+    public class CatContext : DbContext
+    {
+        public DbSet<Cat> Cats { get; set; }
+        public CatContext(DbContextOptions<CatContext> options) : base(options)
+        {
+
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Cat>()
+                 .HasKey(c => c.Id);
+            modelBuilder.Entity<Cat>()
+                .Property(c => c.Name)
+                .IsRequired()
+                .HasMaxLength(30);
+            modelBuilder.Entity<Cat>()
+                .Property(c => c.Age)
+                .IsRequired();
+            modelBuilder.Entity<Cat>()
+                .Property(c => c.Breed)
+                .IsRequired()
+                .HasMaxLength(30);
+        }
+    }
+}
---
+++ Cat/DLL/Repositoy/CatRepository.cs
@@ -0,0 +1,50 @@
+﻿using DLL.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DLL.Repositoy
+{
+    public class CatRepository
+    {
+        private readonly CatContext _context;
+        public CatRepository(CatContext context)
+        {
+            _context = context;
+        }
+        public async Task<Cat> Add(Cat cat)
+        {
+            _context.Cats.Add(cat);
+            await _context.SaveChangesAsync();
+            return cat;
+        }
+        public async Task<Cat> Delete(int id)
+        {
+            var cat = await GetById(id);
+            if (cat == null)
+            {
+                return null;
+            }
+            _context.Cats.Remove(cat);
+            await _context.SaveChangesAsync();
+            return cat;
+        }
+        public async Task<Cat> Update(Cat cat)
+        {
+            _context.Cats.Update(cat);
+            await _context.SaveChangesAsync();
+            return cat;
+        }
+        public async Task<Cat> GetById(int id)
+        {
+            return await _context.Cats.FindAsync(id);
+        }
+        public async Task<List<Cat>> GetAll(int limit = 10, int offset = 0)
+        {
+            return await _context.Cats.ToListAsync();
+        }
+    }
+}

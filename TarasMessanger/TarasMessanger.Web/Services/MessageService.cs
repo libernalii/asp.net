9f935fdcc3efd807edd3c5457012d377552237a6
dmytroprog <dmytroprog@gmail.com>
2026-03-09 18:34:34 +0100

    message service

---
+++ TarasMessanger/TarasMessanger.Web/Services/MessageService.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TarasMessanger.Storage;
+using TarasMessenger.Core.Models.Messages;
+
+namespace TarasMessanger.Web.Services;
+
+public class MessageService
+{
+    private DataContext _context;
+
+    public MessageService(DataContext context)
+    {
+        _context = context;
+    }
+
+    public async Task AddMessage(MessageBase message)
+    {
+        _context.MessageBases.Add(message);
+        await _context.SaveChangesAsync();
+    }
+    
+    public Task<List<MessageBase>> GetMessages(Guid chatId, int limit, int offset)
+    {
+        return _context.MessageBases
+            .AsNoTracking()
+            .Where(x => !x.IsDeleted)
+            .OrderByDescending(x => x.SendAt)
+            .Skip(offset)
+            .Take(limit)
+            .ToListAsync();
+    }
+}

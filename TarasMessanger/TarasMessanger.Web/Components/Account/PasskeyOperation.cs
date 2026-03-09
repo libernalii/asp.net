3c0a5806a267d56a0cf2464bc62d8d52719dbf98
dmytroprog <dmytroprog@gmail.com>
2026-03-09 17:26:29 +0100

    init

---
+++ TarasMessanger/TarasMessanger.Web/Components/Account/PasskeyOperation.cs
@@ -0,0 +1,7 @@
+namespace TarasMessanger.Web.Components.Account;
+
+public enum PasskeyOperation
+{
+    Create = 0,
+    Request = 1,
+}
---
+++ TarasMessanger/TarasMessanger.Web/Data/ApplicationUser.cs
@@ -0,0 +1,8 @@
+using Microsoft.AspNetCore.Identity;
+
+namespace TarasMessanger.Web.Data;
+
+// Add profile data for application users by adding properties to the ApplicationUser class
+public class ApplicationUser : IdentityUser
+{
+}

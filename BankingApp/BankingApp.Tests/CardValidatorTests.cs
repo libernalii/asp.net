a89b6c536dbf4f503aba7944c5b5ca1b38ab68f0
libernalii <alinaliberna@gmail.com>
2026-03-20 20:24:41 +0200

    BankApp

---
+++ BankingApp/BankingApp.Tests/CardValidatorTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankingApp.Tests
+{
+    public class CardValidatorTests
+    {
+        private readonly CardValidator _validator = new CardValidator();
+
+        [Fact]
+        public void ValidCard_ReturnsTrue()
+        {
+            Assert.True(_validator.IsValid("4539578763621486"));
+        }
+
+        [Fact]
+        public void InvalidCard_ReturnsFalse()
+        {
+            Assert.False(_validator.IsValid("1234567890123456"));
+        }
+    }
+}
---
+++ BankingApp/BankingApp/DepositCalculator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankingApp
+{
+    public class DepositCalculator
+    {
+        public decimal Calculate(decimal amount, int months, decimal yearlyRate = 0.1m)
+        {
+            decimal monthlyRate = yearlyRate / 12;
+
+            for (int i = 0; i < months; i++)
+            {
+                amount += amount * monthlyRate;
+            }
+
+            return amount;
+        }
+    }
+}
